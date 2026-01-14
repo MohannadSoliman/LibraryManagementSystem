df68d4b3a540a1eaf0b74c32e1e46661da1afcd8
Mohannad Soliman <mohannadsoliman12@gmail.com>
2026-01-14 21:11:27 +0200

    Adding documentation and cleaning README

---
+++ Documentation/Database/init.sql
@@ -0,0 +1,39 @@
+CREATE TABLE books (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  isbn VARCHAR(50) UNIQUE NOT NULL,
+  available_quantity INT DEFAULT 0,
+  shelf_location VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE borrowers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  registered_date DATE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE borrowings (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  book_id INT NOT NULL,
+  borrower_id INT NOT NULL,
+  borrowed_date DATE NOT NULL,
+  due_date DATE NOT NULL,
+  returned_date DATE,
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (borrower_id) REFERENCES borrowers(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_books_title ON books(title);
+CREATE INDEX idx_books_author ON books(author);
+CREATE INDEX idx_books_isbn ON books(isbn);
+CREATE INDEX idx_borrowers_name ON borrowers(name);
+CREATE INDEX idx_borrowings_borrower_id ON borrowings(borrower_id);
+CREATE INDEX idx_borrowings_book_id ON borrowings(book_id);
+CREATE INDEX idx_borrowings_due_date ON borrowings(due_date);
+
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# LibraryManagementSystem
-Library management system to manage books and borrowers.
+# Library Management System
+
+A professional Node.js API to manage library operations, including books, borrowers, borrowings, and reporting. Fully Dockerized for quick setup and testing.
+
+## Features
+- Manage books and borrowers
+- Checkout and return multiple books
+- Track overdue books automatically
+- Export reports (CSV/XLSX)
+- In-memory caching for faster API responses
+
+## Prerequisites
+- Node.js v18+
+- npm
+- Docker & Docker Compose
+- Git
+
+## Setup & Run
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/MohannadSoliman/LibraryManagementSystem.git
+   ```
+2. Create a .env file in the project root like the on in `Documentation` folder.
+
+3. Run with Docker Compose:
+
+    ```bash
+    docker-compose up --build
+    ```
+4. Access the API at http://localhost:3000
+
+## Documentation
+Full API documentation and database schema can be found in the `Documentation` folder.
